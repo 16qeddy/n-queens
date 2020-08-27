404cfa246040efca5b860b89c83e0c591c22f528
charles Eddy <16qeddy@gmail.com>
2020-08-26 21:25:03 -0700

    initialized the rook counter solution

--- src/solvers.js
+++ src/solvers.js
@@ -11,20 +11,68 @@
 // take a look at solversSpec.js to see what the tests are expecting
 
 
+
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-
-
-
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+
+  var solution = new Board({n: n});
+
+  var matrix = solution.rows();
+
+  var placingIndex = 0;
+
+  for (var row = 0; row < matrix.length; row++) {
+    matrix[row][placingIndex] = 1;
+    placingIndex++;
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return matrix;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  //to refactor consider an array of colum and row indexes that you can keep track of the ones we've visited. .contains or .includes
+
+  var solutionCount = 0;
+  var storage = {};
+  // create a matrix
+  var newBoard = new Board({n: n});
+  // create a new board
+  var matrix = newBoard.rows();
+
+  // add a third for loop for other cases
+
+  // number of set pieces
+  var setPieces = 0;
+  // iterate over rows
+  for (var row = 0; row < matrix.length; row++) {
+    // iterate over columns
+    for (var col = 0; col < matrix.length; col++) {
+      // place a 1 here
+      matrix[row][col] = 1;
+      setPieces++;
+
+      // if row or conflict are true, can skip current (rest of) row or column
+
+      // if current position passes both rows and cols conflict checks
+      if (newBoard.hasRowConflictAt(row) || newBoard.hasColConflictAt(col)) {
+        matrix[row][col] = 0;
+        setPieces--;
+      }
+    }
+  }
+
+  console.log('matrix before setting key', matrix);
+  if (setPieces === 4) {
+    var storageKey = JSON.stringify(matrix);
+    if (storage[storageKey] === undefined) {
+      solutionCount++;
+      storage[storageKey] = true;
+    }
+  }
+
+  console.log(storage, '==============');
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
